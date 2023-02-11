d4feba2b94a7f9138c5633b7a358b901569434e5
DavitAndzavelidze <davit.andzavelidze@gmail.com>
2023-02-11 10:42:55 +0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
-
-// import App from './App'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
